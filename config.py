e8996d52b33e4e7e0779f3ac38a2824662e7240e
maksimssaripov63 <103312625+maksimssaripov63@users.noreply.github.com>
2023-06-15 12:31:05 +0300

    first publish
    есть 2 клавиатуры ReplyMarkup и Inline

---
+++ config.py
@@ -0,0 +1,6 @@
+bot_token = "5650914925:AAFjwZD5g6HqnTcOLXNt9naq2LigE7JWosI"
+chat_id = 996033461
+
+URL_IPHONE13 = 'https://www.apple.com/shop/buy-iphone/iphone-13-pro'
+URL_MACBOOK  = 'https://www.apple.com/shop/buy-mac/macbook-pro/16-inch'
+URL_iPad = 'https://www.apple.com/ipad/'
